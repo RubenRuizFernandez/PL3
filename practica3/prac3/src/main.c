f873ad8830a2d693d803cf2e657d994fbde98115
dasiUAH <75973341+dasiUAH@users.noreply.github.com>
2022-03-30 09:10:29 +0200

    Update main.c

--- practica3/prac3/src/main.c
+++ practica3/prac3/src/main.c
@@ -11,18 +11,17 @@ int main()
 
  //Instalar como manejador del trap 0x83 la rutina
 // que habilita las interrupciones
- leon3_install_handler(0x83,leon3_trap_handler_enable_irqs);
+ leon3_install_handler(0x83, leon3_trap_handler_enable_irqs);
  //Instalar el manejador del trap que 0x83 la rutina
 // que deshabilita las interrupciones
- leon3_install_handler(0x84,
- leon3_trap_handler_disable_irqs);
+ leon3_install_handler(0x84, leon3_trap_handler_disable_irqs);
 
  //Deshabilitar las interrupciones
  leon3_sys_call_disable_irqs();
 //Instalar la función hw_irq_vector_0x11_handler como
 // manejador de la interrupción de nivel 1
 //COMPLETAR
- leon3_install_handler(0x11,hw_irq_vector_0x11_handler);
+ leon3_install_handler(0x11,hw_irq_vector_0x11_handler);  // Estas usando los ficheros de este curso???, es la irq 1
 
  //Desenmascarar la interrupcion de nivel 1 (correspondiente
  //al vector 17, es decir el vector 0x11)
@@ -35,7 +34,7 @@ int main()
  leon3_sparc_force_irq(1);
 
 //DIVISION POR 0
- leon3_install_handler(0x82,leon3_trap_handler_enable_irqs);
+ leon3_install_handler(0x82,leon3_trap_handler_enable_irqs);  // Esto que hace ???, que pasa al dividir entre cero?
  uint8_t i;
   uint8_t j;
   for(i=10; i>0; i--)
